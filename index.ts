7396254a21026491a46d7da4375a6b76bb691010
KirPot <kiran@dev.com>
2024-06-21 12:25:38 +0530

    log error

--- index.ts
+++ index.ts
@@ -20,6 +20,7 @@ export default async function handler(req: Request, res: Response) {
       .status(200)
       .json({ message: "Generated the summarization", summarization });
   } catch (err) {
+    console.log(err);
     return res.status(500).json({ message: "Internal server error" });
   }
 }
